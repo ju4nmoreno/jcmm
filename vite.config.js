080001c0bd71e2164b8146fee44434fa7e933e11
juan moreno <jcmm.info@gmail.com>
2023-10-04 07:12:19 -0500

    add new project

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
 const isCodeSandbox =
-  "SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env;
+  'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
 
 export default {
-  root: "src/",
-  publicDir: "../static/",
-  base: "./",
+  root: 'src/',
+  publicDir: '../static/',
+  base: './',
   server: {
     host: true,
-    open: !isCodeSandbox, // Open if it's not a CodeSandbox
+    open: !isCodeSandbox // Open if it's not a CodeSandbox
   },
   build: {
-    outDir: "../docs",
+    outDir: '../docs',
     emptyOutDir: true,
-    sourcemap: true,
-  },
-};
+    sourcemap: true
+  }
+}
